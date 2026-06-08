132a0c9670ffdbffed12bcd7af50a8c89e30e4c3
Piyush Bhul <130152976+Piyush3024@users.noreply.github.com>
2026-06-08 13:04:58 +0545

    feat: add fallback image component

---
+++ src/components/shared/image-with-fallback.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Image, { type ImageProps } from 'next/image';
+import { useEffect, useState } from 'react';
+
+import NoImgFallback from '@/assets/no-image.png';
+
+export interface ImageWithFallbackProps extends Omit<ImageProps, 'src' | 'onError'> {
+  src?: string | null;
+  fallbackSrc?: string;
+}
+
+export function ImageWithFallback({
+  src,
+  fallbackSrc = NoImgFallback.src,
+  ...props
+}: ImageWithFallbackProps) {
+  const [imgSrc, setImgSrc] = useState<string>(src ?? fallbackSrc);
+
+  useEffect(() => {
+    setImgSrc(src ?? fallbackSrc);
+  }, [src, fallbackSrc]);
+
+  const handleError = () => {
+    if (imgSrc !== fallbackSrc) {
+      setImgSrc(fallbackSrc);
+    }
+  };
+
+  return <Image {...props} onError={handleError} src={imgSrc} />;
+}
